caeeffd4371b81ad53a93c1b9b41b06bd05b4f80
Ebin Varghese <108483840+Techie-NEWBIE@users.noreply.github.com>
2023-02-06 14:00:20 +0530

    Update binary.c

--- binary.c
+++ binary.c
@@ -25,18 +25,6 @@ for(i=0;i<n;i++)
 scanf("%d",&a[i]);
 }
 
-for(i=0;i<n;i++)
-{
-for(j=0;j<n-i-1;j++)
-{
-if(a[j]>a[j+1])
-{
-temp=a[j];
-a[j]=a[j+1];
-a[j+1]=temp;
-}
-}
-}
 printf("sorted array:\n");
 for(i=0;i<n;i++)
 {
